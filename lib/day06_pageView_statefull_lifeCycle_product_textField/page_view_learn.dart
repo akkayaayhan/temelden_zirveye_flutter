84b409dfb63c8b7611d2f5a2f0f90cc2fb359e34
akkayasoft <akkayasoft@gmail.com>
2023-07-06 19:41:21 +0300

    ilk 7 ders bitti

---
+++ lib/day06_pageView_statefull_lifeCycle_product_textField/page_view_learn.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../day03_icon_color_button_appBar/icon_learn.dart';
+import '../day04_image_padding_stateless_card/image_learn.dart';
+import '../day05_custom_column_row_stack_indicator_listTile/stack_learn.dart';
+
+class PageViewLearn extends StatefulWidget {
+  const PageViewLearn({Key? key}) : super(key: key);
+
+  @override
+  State<PageViewLearn> createState() => _PageViewLearnState();
+}
+
+class _PageViewLearnState extends State<PageViewLearn> {
+  final _pageController = PageController(viewportFraction: 0.7);
+
+  int _currentPageIndex = 0;
+  void _updatePageIndex(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(_currentPageIndex.toString()),
+          ),
+          const Spacer(),
+          FloatingActionButton(
+            onPressed: () {
+              _pageController.previousPage(
+                  duration: _DurationUtility._durationLow,
+                  curve: Curves.slowMiddle);
+            },
+            child: const Icon(Icons.chevron_left),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              _pageController.nextPage(
+                  duration: _DurationUtility._durationLow,
+                  curve: Curves.slowMiddle);
+            },
+            child: const Icon(Icons.chevron_right),
+          ),
+        ],
+      ),
+      appBar: AppBar(),
+      body: PageView(
+        padEnds: false,
+        controller: _pageController,
+        onPageChanged: _updatePageIndex,
+        children: [
+          const ImageLaern(),
+          IconLearnView(),
+          const StackLearn(),
+        ],
+      ),
+    );
+  }
+}
+
+class _DurationUtility {
+  static const _durationLow = Duration(seconds: 1);
+}
