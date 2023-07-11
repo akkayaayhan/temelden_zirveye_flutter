06e99bbb7995ac4190798ae2d96eacde104dc38b
akkayasoft <akkayasoft@gmail.com>
2023-07-11 05:42:53 +0300

    değişiklikler işlendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:temelden_zirveye_flutter/random_image.dart';
 
 import 'day07_lifeCycle_listView_navigation_demos/list_view_builder.dart';
+import 'day08_model_tabbar_nullable/tab_learn.dart';
+import 'day09_postman_servis_future_try_catch/coments_learn_view.dart';
 import 'day09_postman_servis_future_try_catch/service_learn_view.dart';
+import 'day09_postman_servis_future_try_catch/service_post_learn_view.dart';
+import 'day10_theme_package_textfieldAdvanced/package_learn_view.dart';
+import 'day10_theme_package_textfieldAdvanced/password_text_field.dart';
+import 'day10_theme_package_textfieldAdvanced/theme_learn_view.dart';
+import 'day11_animasyonlar_extension_oop_imageAdvanced/animated_learn_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +22,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ServiceLearn(),
+      theme: ThemeData.dark(),
+      home: const AnimatedLearnView(),
     );
   }
 }
